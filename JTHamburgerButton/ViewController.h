b018364f76aaef9a75084fc0620ac08244cd25f3
Jonathan Tribouharet <jonathan@siu-soon.com>
2015-03-22 19:34:49 -0400

    Initial Commit

---
+++ JTHamburgerButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JTHamburgerButton
+//
+//  Created by Jonathan on 22/03/15.
+//  Copyright (c) 2015 Jonathan Tribouharet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
